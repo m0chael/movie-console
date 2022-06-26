9d98f8bb87971f41b75c4d86b30a820e3c50b3cf
m0chael <michael.stockermans+github@gmail.com>
2022-06-26 11:36:38 -0400

    Styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,9 +48,10 @@ const Home = (props) => {
     const render_results = () => {
         if (movie_results && movie_results.length == 0) {
             return <div className="no-results">No results</div>
-
         } else if (movie_results && movie_results.length > 0) {
             return movie_results[0].show ? <ShowGrid data={movie_results}></ShowGrid> : <ActorGrid data={movie_results}></ActorGrid>
+        } else {
+            return  <div className="no-results">Please search for a movie or actor...</div>
         }
     };
     
@@ -62,18 +63,18 @@ const Home = (props) => {
         <MainPageLayout>
             <div className="main-console">
                 <Title title="Movie console" subtitle="Search for a movie or actor!"></Title>
-                <input className="main-search" type="text" placeholder="Search for movies..." onKeyDown={on_key_down} onChange={on_input_change} value={input}></input>
+                <input className="main-search" type="text" placeholder="Search..." onKeyDown={on_key_down} onChange={on_input_change} value={input}></input>
                 <div className="radios">
                         <label htmlFor="shows-search">
                             Shows
                             <input id="shows-search" checked={is_shows_searching} type="radio" value="shows" onChange={on_radio_change}></input>
                         </label>
                         <label htmlFor="actors-search">
-                            Actor
+                            Actors
                             <input id="actors-search" checked={!is_shows_searching} type="radio" value="people" onChange={on_radio_change}></input>
                         </label>
                 </div>
-                <button type="button" onClick={on_search}>Search</button>
+                <button type="button" onClick={on_search}>Search now</button>
            </div>
            { render_results() }
         </MainPageLayout>
